5ca35e5a8faf931d36024f29962dee88e891e13b
Annisa Hadi <annisa.hadi@gmail.com>
2018-09-23 14:13:19 -0700

    test the delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -51,23 +51,21 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.delete('/todos/:id', (req, res) => {
-	var id = req.params.id;
+  var id = req.params.id;
 
-	if(!ObjectID.isValid(id)){
-		res.status(404).send();
-	}
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-	Todo.findByIdAndRemove({
-		_id : id
-	}).then((deletedTodo) => {
-		if(!deletedTodo){
-			return res.status(404).send();
-		}
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-		res.send(deletedTodo);
-	}).catch((e) => {
-		res.status(400).send();
-	})
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 
 })
 
