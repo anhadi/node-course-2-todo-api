dc5561716a897b26a33fc6ac7f88a166f3a1559d
Annisa Hadi <annisa.hadi@gmail.com>
2018-09-29 10:22:21 -0700

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -33,7 +33,7 @@ const _ = require('lodash');
   }
  )
 
- UserSchema.methods.toJSON =  function  () {
+ UserSchema.methods.toJSON =  function () {
  	var user = this;
  	var userObject = user.toObject();
 
@@ -52,7 +52,23 @@ const _ = require('lodash');
  	});
  };
 
-var User = mongoose.model('User', UserSchema
-);
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = (process.env.PORT );
@@ -100,11 +101,7 @@ app.patch('/todos/:id', (req, res) => {
 
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
-
-  var user = new User({
-    email: body.email, 
-    password: body.password
-  });
+  var user = new User(body);
 
   user.save().then(() => {
     return user.generateAuthToken();
@@ -112,12 +109,12 @@ app.post('/users', (req, res) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
-  });
+  })
+});
 
-})
-
-
-
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 
 
